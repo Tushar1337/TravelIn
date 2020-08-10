a1b69b5c765a1cbe95ca8f8a2af6c2682478b62f
Tushar Raina <tusharraina46@gmail.com>
2020-08-10 21:00:08 +0530

    Added a function to display Reviews

--- travel/website/admin.py
+++ travel/website/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Destination,SpecialOffer,Packages
+from .models import Destination,SpecialOffer,Packages,Reviews
 
 admin.site.register(Destination)
 admin.site.register(SpecialOffer)
 admin.site.register(Packages)
+admin.site.register(Reviews)
